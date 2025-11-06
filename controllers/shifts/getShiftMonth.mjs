2f9c648fafdf2b49e99d6aca263aa80c26a7b2b2
Maxycarp89 <maxicostilla21@gmail.com>
2025-11-06 08:34:44 -0300

    update: add missing fields to TURNOS query and include logo image

--- controllers/shifts/getShiftMonth.mjs
+++ controllers/shifts/getShiftMonth.mjs
@@ -6,7 +6,7 @@ const getShiftMonth = async(req,res) =>{
 
 try {
   const shiftsExist = await clienteAxios.get(
-    `/b1s/v1/TURNOS?$filter=(U_Fecha ge '${FechInicio}' and U_Fecha le '${FechFinal}')and U_BPLId eq ${BPLId}&$select=DocEntry,CreateDate,U_BPLId,U_customer,U_custmrName,U_Telephone,U_Street,U_City,U_subject,U_status,U_callType,U_priority,U_problemTyp,U_descrption,U_ProSubType,U_origin,U_BPType,U_itemCode,U_itemName,U_Chasis,U_Motor,U_State,U_Fecha,U_internalSN,U_StartTime&$orderby=U_Fecha desc`,
+    `/b1s/v1/TURNOS?$filter=(U_Fecha ge '${FechInicio}' and U_Fecha le '${FechFinal}')and U_BPLId eq ${BPLId}&$select=DocEntry,U_dni,CreateDate,U_BPLId,U_customer,U_custmrName,U_Telephone,U_Street,U_City,U_subject,U_status,U_callType,U_priority,U_problemTyp,U_descrption,U_ProSubType,U_origin,U_BPType,U_itemCode,U_itemName,U_Chasis,U_Motor,U_State,U_Fecha,U_internalSN,U_StartTime&$orderby=U_Fecha desc`,
     {
       headers: {
         Cookie: `${cookies}`,
